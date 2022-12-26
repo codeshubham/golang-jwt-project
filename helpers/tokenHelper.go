2196415ea7773922e4620ff7ebc6dc5da5a1d1be
shubham kumar <shubhammank@gmail.com>
2022-12-26 21:38:48 +0530

    completed

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -53,6 +54,33 @@ func GenerateAllTokens(email string, first_name string, last_name string, userTy
 	}
 
 	return token, refreshToken, err
+}
+
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(SECRET_KEY), nil
+		},
+	)
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = fmt.Sprintf("the token is invalid")
+		msg = err.Error()
+		return
+	}
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = fmt.Sprintf("token is expired")
+		msg = err.Error()
+		return
+	}
+	return claims, msg
 }
 
 func UpdateAllTokens(signedToken string, signedRefreshToken string, userID string) {
--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	routes "golang-jwt-project/routes"
 	"os"
+
+	routes "gitub.com/codeshubham/golang-jwt-project/routes"
 
 	"github.com/gin-gonic/gin"
 )
